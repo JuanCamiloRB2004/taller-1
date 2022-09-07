1b655598d5389bcc9406bdf0544af177e4681309
JuanCamiloRB2004 <juanc.ramirezb@autonoma.edu.co>
2022-09-07 15:27:58 -0500

    punto A

---
+++ main.py
@@ -0,0 +1,7 @@
+'''
+    author: JuanCamilo Ramirez Buritica
+    date: 07/09/2022
+    verssio: 1.0
+'''
+from supermercado import supermercado
+supermercado =supermercado()
---
+++ supermercado.py
@@ -0,0 +1,8 @@
+class supermercado():
+    def __init__(self):
+        self.produtos_lacteos = []
+        self.productos_aseo = []
+        self.productos_granos = []
+    
+        
+    
