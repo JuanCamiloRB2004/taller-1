6eec5344f45c729939feea12dc83ea780c76d30b
JuanCamiloRB2004 <juanc.ramirezb@autonoma.edu.co>
2022-09-07 15:37:46 -0500

    Supermercado.py file

--- supermercado.py
+++ supermercado.py
@@ -3,6 +3,13 @@ class supermercado():
         self.produtos_lacteos = []
         self.productos_aseo = []
         self.productos_granos = []
+        
+        self.productos_aseo_existencia =[]
+        self.produtos_lacteos_existencia = []
+        self.productos_granos_existecia = []
+        
+    def ingresar_prouctos():
+        
     
         
     
